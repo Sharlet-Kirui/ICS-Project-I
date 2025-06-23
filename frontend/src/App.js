f850294c03997c06ee30e5bb42cfc069ebeadc75
Sharlet Kirui <sharlet.kirui@gmail.com>
2025-06-23 15:36:40 +0300

    Routing for documents

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/global/navbar';
 import SignUp from './components/global/sign_up';
 import Details from './components/startup/details';
+import Documents from './components/startup/documents';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={<h1>Welcome to the Homepage</h1>} />
         <Route path="/details" element={<Details />} />
+        <Route path="/documents" element={<Documents/>} />
       </Routes>
     </Router>
   );
